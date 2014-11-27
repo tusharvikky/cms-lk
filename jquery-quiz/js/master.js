69297b29404c8f7594f3eb6ffca298cdb34abfc1
tusharvikky <tusharvikky@gmail.com>
2014-11-27 19:09:03 +0530

    [ADDED] Library slickQuiz, Quiz functional [PENDING] json Data

---
+++ jquery-quiz/js/master.js
@@ -0,0 +1,10 @@
+// Put all your page JS here
+
+$('#slickQuiz').slickQuiz({
+	'numberOfQuestions':   3,
+	'randomSortQuestions': true,
+	'randomSortAnswers':   true,
+	'perQuestionResponseMessaging': false,
+	
+
+});
